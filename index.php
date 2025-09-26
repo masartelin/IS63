71cce91d52bf19f1e37bdd226807a53f930e490a
masartelin <dimasdawahyu06@gmail.com>
2025-09-26 11:01:32 +0700

    dasar php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar web programing</title>
+</head>
+<body>
+    <?php
+        // echo "hello world";
+
+        //variabel
+        $nilai = 75;
+        $nilai2 = 80;
+        $nama = "Budiman";
+        $alamat = "Banda Aceh, lampenerut";
+        $alamatsekolah = "SMA 4 B.Aceh";
+        $rata2 = ($nilai + $nilai2) / 2;
+
+        
+
+
+        echo "<h1>Nama: $nama <br> Alamat Anda: $alamat <br> Alamat Skolah: $alamatsekolah <br> Nilai Rata-Rata: $rata2 </h1>";
+
+        // ATURAN MEMBUAT VARIABEL di PHP
+        // 1. tidak boleh diawali oleh angka atau simbol kecuali _ underscore
+        // 2. tidak boleh pakai spasi
+        // 3. variable bersifat case sensitive
+
+        // OPERATOR ARITMATIKA (+ - * / ** %)
+
+    ?>
+
+
+
+
+
+
+</body>
+</html>
